cd1b886634659b9affe779df483ace15fa8e24d9
marti <marti@192.168.0.238>
2022-08-23 15:32:33 -0300

    Ejercicio Sumados

---
+++ EjerciciosRepaso/src/ejerciciosRepaso/Ejercicio_29.java
@@ -0,0 +1,41 @@
+package ejerciciosRepaso;
+
+/*Escribir una función consonantes que recibe una cadena de caracteres
+  y devuelve la cadena que resulta de eliminar todas las vocales de la cadena recibida. Por ejemplo:
+ consonantes("hola como estas"); // devuelve "hl cm sts"*/
+
+public class Ejercicio_29 {
+
+	public String consonante(String str) {
+
+		String n1;
+
+		n1 = str.replace("a", "");
+		n1 = n1.replace("e", "");
+		n1 = n1.replace("i", "");
+		n1 = n1.replace("o", "");
+		n1 = n1.replace("u", "");
+
+		return n1;
+	}
+
+	String consonantes(String str) {
+
+		String vocales = "aeiouAEIOU";
+		String sinVocales = str;
+
+		for (int i = 0; i < vocales.length(); i++) {
+			sinVocales = sinVocales.replace(vocales.charAt(i), ' ');
+		}
+		return sinVocales;
+	}
+
+	public static void main(String[] args) {
+
+		Ejercicio_29 a = new Ejercicio_29();
+
+		System.out.println(a.consonantes("Holacomoestas"));
+
+	}
+
+}
--- Sumados/src/sumados/Sumados.java
+++ Sumados/src/sumados/Sumados.java
@@ -6,7 +6,7 @@ public class Sumados {
 
 		int suma = 0;
 
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 1; i < 1000; i++) {
 
 			suma += i;
 
